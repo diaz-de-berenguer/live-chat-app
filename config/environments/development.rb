d0a215194dafcf81fd6b8121910aff1c5efb6e4b
Diego Diaz de Berenguer <d.diazdeberenguer@gmail.com>
2018-04-25 13:56:20 -0500

    Allow connections to Action Cable from any url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Allows requests from any domain
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
