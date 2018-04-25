59b2495fbf1f554fcf9fa1864432c67f4414a7a6
Diego Diaz de Berenguer <d.diazdeberenguer@gmail.com>
2018-04-25 13:28:38 -0500

    Add action cable to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+	# Action Cable will be listening for WebSocket requests on ws://localhost:3000/cable
+	mount ActionCable.server => '/cable'
   root 'home#index'
 end
